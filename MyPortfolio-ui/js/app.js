533217241504b89653a3aaeceab21104e8725da1
pionayyy11 <fionasamanthae.gmail.com>
2026-01-21 10:43:59 +0800

    Update

---
+++ MyPortfolio-ui/js/app.js
@@ -0,0 +1,9 @@
+
+// MyPortfolio-ui/js/app.js
+console.log("Hello from MyPortfolio UI!");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const heading = document.querySelector("h1");
+  heading.style.color = "blue"; // changes the heading color dynamically
+});
+
